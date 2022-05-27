846065a993384d757b76560098c9df2c2ecf4e15
cfyjoanna <joannachanfy@gmail.com>
2022-05-28 01:00:27 +0800

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
@@ -8,11 +18,13 @@ import Home from './pages/Home/Home';
 import Preferences from './pages/Preferences/Preferences';
 import Login from './pages/Login';
 
+
 function App() {
   const { user } = useAuth();
 
   return (
     <div>
+      <MenuAppBar />
       {user ? <Home /> : <Login />}
       <BrowserRouter>
         <Routes>
@@ -23,6 +35,78 @@ function App() {
       </BrowserRouter>
     </div>
   );
+
+  function MenuAppBar() {
+    const { user, signout } = useAuth();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  
+    const handleLogout = () => {
+      handleClose();
+      signout();
+    };
+  
+    const handleMenu = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              modNUS
+            </Typography>
+            {user && (
+              <div>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={handleClose}>My account</MenuItem>
+                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  }
 }
 
 export default App;
