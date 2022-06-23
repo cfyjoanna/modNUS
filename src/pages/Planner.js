0ea0cfebfe99ea9477ce7629acd77fb0e625d2a0
cfyjoanna <joannachanfy@gmail.com>
2022-06-23 14:37:07 +0800

    integrated firestore for y1s2

--- src/pages/Planner.js
+++ src/pages/Planner.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import SearchModules from './SearchModules';
 import PlannerMods from './components/PlannerMods';
 import { useAuth } from '../hooks/useAuth';
 import { db } from '../config/firebaseConfig';
 import { doc, updateDoc, getDoc } from 'firebase/firestore';
 
 var y1s1 = [];
+var y1s2 = [];
 
 export default function Planner() {
   const { user } = useAuth();
@@ -20,11 +20,20 @@ export default function Planner() {
     });
   }
 
+  const updateY1s2 = async (mods) => {
+    const docRef = doc(db, "users", user.uid);
+
+    await updateDoc(docRef, {
+      y1s2: mods
+    });
+  }
+
   const getMods = async () => {
     const docRef = doc(db, "users", user.uid);
     const docSnap = await getDoc(docRef);
 
     y1s1 = docSnap.data().y1s1;
+    y1s2 = docSnap.data().y1s2;
   }
   getMods().then(result => setFinish(true));
   
@@ -36,7 +45,7 @@ export default function Planner() {
       <PlannerMods updater={updateY1s1} mods={y1s1} />
 
       <h3>Y1S2</h3>
-      <SearchModules />
+      <PlannerMods updater={updateY1s2} mods={y1s2} />
     </div>
   );
 }
