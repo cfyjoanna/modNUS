ad62c34bdd7fc1ba5a4c8d61100155c745bf6cc5
cfyjoanna <joannachanfy@gmail.com>
2022-07-23 00:04:18 +0800

    added past reviews to profile

--- src/pages/PastReviews.js
+++ src/pages/PastReviews.js
@@ -4,11 +4,18 @@ import { Grid } from '@mui/material';
 
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
+
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import ArticleIcon from '@mui/icons-material/Article';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+
+import Tooltip from '@mui/material/Tooltip';
+import Rating from '@mui/material/Rating';
+import LabelledRatingReadOnly from './components/LabelledRatingReadOnly.js';
 
 import { Link } from 'react-router-dom';
 import { collection, getDocs, query, where } from 'firebase/firestore'; 
@@ -33,8 +40,6 @@ export default function PastReviews() {
     getReviews().then(result => setFinish(true));
     // eslint-disable-next-line
   }, []);
-
-  console.log(reviews);
 
   return(
     <div className="wrapper">
@@ -67,7 +72,52 @@ export default function PastReviews() {
 
         </Grid>
         <Grid item xs={9}>
-          <h3>Module reviews (to be replaced)</h3>
+          {/* Printing out reviews from database*/}
+          {reviews.map((review) => {
+            return(
+              <div className="review-box" key={review.key}>
+                <Grid container alignItems="center" spacing={2}>
+                  <Grid item xs={11}>
+                    <span style={{fontWeight: 'bold'}}>{review.module}</span>
+                    <span> review by {review.user}</span>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <Tooltip title="Delete review">
+                      <IconButton aria-label="Example">
+                        <DeleteIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <span>Taken in {review.sem} under {review.prof}</span>
+                  </Grid>
+
+                  <Grid item xs={1}>
+                    <span style={{fontWeight: 'bold'}}>Overall:</span>
+                  </Grid>
+                  <Grid item xs={11}>
+                    <Rating name="read-only" value={review.rating} readOnly />
+                  </Grid>
+                  <Grid item xs={1}>
+                    <span style={{fontWeight: 'bold'}}>Difficulty:</span>
+                  </Grid>
+                  <Grid item xs={11}>
+                    <LabelledRatingReadOnly value={review.difficulty} />
+                  </Grid>
+                  <Grid item xs={1}>
+                    <span style={{fontWeight: 'bold'}}>Workload:</span>
+                  </Grid>
+                  <Grid item xs={11}>
+                    <LabelledRatingReadOnly value={review.workload} />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    {review.text}
+                  </Grid>
+                </Grid>
+              </div>
+            )
+          })}
         </Grid>
       </Grid>
     </div>
